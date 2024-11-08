1537e9fa62c1e161098c1f082fb09bd5a74016e6
Terry <68700354+teraprath@users.noreply.github.com>
2024-11-08 17:00:48 +0100

    Add Regions

---
+++ src/main/java/com/github/teraprath/tinylib/region/Region.java
@@ -0,0 +1,27 @@
+package com.github.teraprath.tinylib.region;
+
+import org.bukkit.Location;
+
+import javax.annotation.Nonnull;
+
+public class Region extends RegionCuboid {
+
+    private final String name;
+    private String displayName;
+
+    public Region(String name, Location pos1, Location pos2) {
+        super(pos1, pos2);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() { return displayName; }
+
+    public void setDisplayName(@Nonnull String string) {
+        this.displayName = string;
+    }
+
+}
